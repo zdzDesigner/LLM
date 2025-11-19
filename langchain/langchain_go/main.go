4d11cb50d5b10500e836ec24f04cbf9b49827269
zdzDesigner <1696498456@qq.com>
2025-11-19 19:57:06 +0800

    ~

--- langchain/langchain_go/main.go
+++ langchain/langchain_go/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	// "time"
 
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai"
@@ -14,6 +15,7 @@ func main() {
 	fmt.Println(key)
 
 	ctx := context.Background()
+	// ctx, cancel := context.WithCancel(context.Background())
 
 	llm, err := openai.New(
 		openai.WithToken(key),
@@ -29,9 +31,29 @@ func main() {
 		llms.TextParts(llms.ChatMessageTypeHuman, "帮我介绍下interface类型"),
 	}
 
+	// go func() {
+	// 	time.Sleep(time.Second * 3)
+	// 	cancel()
+	// }()
+	go func() {
+		select {
+		case <-ctx.Done():
+			fmt.Println("OK")
+			// default:
+			// 	time.Sleep(time.Second * 1)
+			// 	fmt.Println("xxxxxxx")
+		}
+	}()
 	// llm.GenerateContent(ctx,messages,func(co *llms.CallOptions) {})
 	res, err := llm.GenerateContent(ctx, messages, llms.WithTemperature(0.7))
-	for _, cc := range res.Choices {
-		fmt.Println(cc.Content)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	fmt.Println(res)
+	// for _, cc := range res.Choices {
+	// 	fmt.Println(cc)
+	// 	// fmt.Println(cc.Content)
+	// }
 }
